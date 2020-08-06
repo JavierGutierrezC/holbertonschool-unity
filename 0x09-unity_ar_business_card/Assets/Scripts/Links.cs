6c73dcb197b3ddeaba232bfaf2c5b3a76059403e
Javier <javier.gutierrez.cruz@gmail.com>
2020-08-06 11:19:46 -0500

    [Fix]updates

---
+++ 0x09-unity_ar_business_card/Assets/Scripts/Links.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Links : MonoBehaviour
+{
+    public void Twitter()
+    {
+        Application.OpenURL("https://twitter.com/javiguti11");
+    }
+    public void GitHub()
+    {
+        Application.OpenURL("https://github.com/JavierGutierrezC");
+    }
+    public void Mail()
+    {
+        Application.OpenURL("mailto:955@holbertonschool.com");
+    }
+    public void Linkdin()
+    {
+        Application.OpenURL("https://www.linkedin.com/in/javier-gutierrez-59a492161/");
+    }
+}
+
